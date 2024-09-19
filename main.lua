6c91d148da517253be1d676b674ad55cbd81b061
zandy <zandy@zandy.xyz>
2024-09-19 13:23:39 -0500

    Splines

--- main.lua
+++ main.lua
@@ -10,11 +10,17 @@ function love.load()
     Hovered = nil
     Selected = nil
 
-    Points = {
-        {x = 20, y = 10, r = 15, hovered = false, mouse_distance = math.huge, best_hovered = false},
-        {x = 40, y = 10, r = 15, hovered = false, mouse_distance = math.huge, best_hovered = false},
-        {x = 60, y = 10, r = 15, hovered = false, mouse_distance = math.huge, best_hovered = false}
-    }
+    Resolution = 50
+    local n_points = 5
+    Points = {}
+    for i = 1, n_points do
+        Points[i] = {x = 20 * i, y = 10, r = 10, hovered = false, mouse_distance = math.huge, best_hovered = false}
+    end
+    Intermediate = {}
+    Curve = {}
+    for i = 1, Resolution do
+        Curve[i] = { x = i, y = i }
+    end
 
     WorldCanvas = null
     width, height = love.graphics.getDimensions()
@@ -70,6 +76,16 @@ function Distance(x1, y1, x2, y2)
     return math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
 end
 
+function lerp(a, b, t)
+    return a + (b - a) * t
+end
+
+function plerp(a, b, t)
+    local x = lerp(a.x, b.x, t)
+    local y = lerp(a.y, b.y, t)
+    return { x = x, y = y}
+end
+
 function love.update(dt)
     Time = love.timer.getTime()
 
@@ -81,10 +97,25 @@ function love.update(dt)
         Selected.node.x = x
         Selected.node.y = y
     end
+
+    for i = 1, (Resolution + 1) do
+        points = Points
+        local t = (i - 1) / Resolution
+        while #points > 1 do
+            reduced = {}
+            for j = 1, (#points - 1) do
+                local p1 = points[j]
+                local p2 = points[j + 1]
+                reduced[j] = plerp(p1, p2, t)
+            end
+            points = reduced
+        end
+        Curve[i] = points[1]
+    end
 end
 
 function DrawPoints(points)
-    for _, point in pairs(points) do
+    for i, point in pairs(points) do
         if point.best_hovered then
             love.graphics.setColor(theme.highlight())
         else
@@ -94,6 +125,11 @@ function DrawPoints(points)
 
         love.graphics.setColor(theme.outline())
         love.graphics.circle("line", point.x, point.y, point.r)
+
+        local x = point.x - love.graphics.getFont():getWidth(tostring(i)) / 2
+        local y = point.y - love.graphics.getFont():getHeight() / 2
+        love.graphics.setColor(theme.outline())
+        love.graphics.print(tostring(i), x, y)
     end
     love.graphics.setColor(theme.white())
 end
@@ -104,10 +140,28 @@ function love.draw()
     love.graphics.clear(theme.grass())
 
     DrawPoints(Points)
+    love.graphics.setColor(theme.highlight())
+    for i = 1, #Points do
+        p1 = Points[i]
+        if i < #Points then
+            p2 = Points[i + 1]
+            -- love.graphics.line(p1.x, p1.y, p2.x, p2.y)
+        end
+    end
 
+    love.graphics.setColor(theme.white())
+    for i = 1, #Curve do
+        p1 = Curve[i]
+        if i < #Curve then
+            p2 = Curve[i + 1]
+            love.graphics.line(p1.x, p1.y, p2.x, p2.y)
+        end
+    end
+
+    love.graphics.setColor(theme.white())
     love.graphics.setCanvas()
     love.graphics.draw(WorldCanvas, 0, 0)
-end
+    end
 
 function love.resize(w, h)
     ResizeHelper(w, h)
